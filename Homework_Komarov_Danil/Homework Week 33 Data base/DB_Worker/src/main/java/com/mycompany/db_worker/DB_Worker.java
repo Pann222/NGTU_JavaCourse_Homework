88bfdeb7a73368feac353263008b9129bc766468
Danil Komarov <134853845+Pann222@users.noreply.github.com>
2023-06-17 00:52:02 +0300

    Homework week 33 has been added.

---
+++ Homework_Komarov_Danil/Homework Week 33 Data base/DB_Worker/src/main/java/com/mycompany/db_worker/DB_Worker.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.db_worker;
+
+/**
+ *
+ * @author Dan20
+ */
+public class DB_Worker {
+
+    public static void main(String[] args) {
+        JFrameDB_Worker dbWorker = new JFrameDB_Worker();
+        dbWorker.setVisible(true);
+    }
+}
